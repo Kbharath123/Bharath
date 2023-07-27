6c03e200801ebe03bd93efb0e8f79b05258a9671
Kbharath123 <kammaribharath225@email.com>
2023-07-27 10:40:02 +0530

    first commit

---
+++ welcome/src/com/welcome/Demo1.java
@@ -0,0 +1,15 @@
+package com.welcome;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("welcome to java world");
+		
+		
+		
+		
+	}
+
+}
